b9aeaf37173223a70cf52db851bc0c3c5e100991
albinaabykanova <abykanovaalbina@gmail.com>
2023-01-05 22:30:56 +0300

    calculate-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React from 'react';
+import store from './store';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class  App extends React.Component {
+
+  constructor() {
+     super()
+     this.state = {
+        out: '0'
+     }
+     this.refOutput = React.createRef()
+  }
+
+  tapeNumber(value) {
+    let currentValue = value 
+    let output = this.refOutput.current
+
+    this.setState( {
+      out: currentValue
+    })
+   
+    if (output.value === '0') {output.value = ''}
+    output.value += currentValue
+
+  }
+
+  tapeOperations(value) {
+     let output = this.refOutput.current
+
+     if (value === 'CE')  {
+       output.value.length === 1 ?  output.value = '0' :
+         output.value = output.value.substring(0, output.value.length - 1)
+    }
+
+    else if (value === 'C') {output.value = '0'}
+    else if (value === '=') {output.value = eval(output.value)}
+
+  }
+  
+
+
+  render() {
+    return(
+       <div className="container">
+           <div className="output">
+              <input ref={this.refOutput} type="text" defaultValue={this.state.out} />
+           </div>
+           <div className='buttons'>
+               {store.buttons.map(item => <button
+               onClick={() => {this.tapeNumber(item.val)}}
+               >{item.val}</button>)}
+               {store.operations.map(item => <button
+               onClick={() => {this.tapeOperations(item.val)}}
+               >{item.val}</button>)}
+           </div>
+       </div>
+    )
+  }
 }
+
 
 export default App;
